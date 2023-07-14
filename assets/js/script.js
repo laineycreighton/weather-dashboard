97a6154431fc76db277fc93fb0e16db24029065e
Lainey Creighton <dev.lainey@gmail.com>
2023-07-14 09:00:09 -0700

    add psuedocode

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+//allow the user to search for a city's weather forecast by using the search bar
+    //use eventListener for the click of the search bar
+//make sure user puts in a correct city
+    //if the user spells a city wrong or doesn't type in a city, let the user know
+// pull a city's weather info using the open weather api key
+    //get access to the open weather api
+    //use the api url
+    //get api url query parameters
+    //get the city that the user typed
+    //use dayjs to display five day forecast begginning on current date
+    //use a for loop to create a card for each day
+//display a city's weather info to the web page in the correct location
+    //use get element by id
+    //use create element
+    //use .textContent to make sure the info is a text
+    //use append to display the info
+//store a list of previously searched cities
+    //use localStorage to setItem
+    //use get element by id (under search bar)
+    //create element for button
+    //add event listener to the button
+    //use localStorage to getItem previously displayed city information 
